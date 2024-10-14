f2e65e968e03e3f5a2736c4f7fc8d2c161b7215a
samesamessa <cloverremidore@gmail.com>
2024-10-14 17:29:46 +0900

    #4 공용 메뉴 설계

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import MainScroll from "./commponent/RfMain/MainScroll.jsx";
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-import ReformBoard from './commponent/RfBoard/ReformBoard.jsx'; // 게시판 컴포넌트
+import Layout from "./Layout.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-export default function App () {
 
-    return (
-        <BrowserRouter>
-            <div className="App">
-                {/* 헤더 메뉴 */}
-                <header>
-                    <nav className="navbar">
-                        <ul>
-                            <li>
-                                <Link to="/">메인</Link> {/* 메인 페이지로 이동 */}
-                            </li>
-                            <li>
-                                <Link to="/bulletin-board">게시판</Link> {/* 게시판 페이지로 이동 */}
-                            </li>
-                        </ul>
-                    </nav>
-                </header>
 
-                {/* 페이지 라우팅 설정 */}
-                <Routes>
-                    <Route path="/" element={<MainScroll />} /> {/* 메인 페이지 */}
-                    <Route path="/bulletin-board" element={<ReformBoard />} /> {/* 게시판 페이지 */}
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
+function App() {
+
+
+  return (
+    <>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout/>}/>
+            </Routes>
+        </Router>
+    </>
+  )
 }
+
+export default App
