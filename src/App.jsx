ba1e86c515b88d5859e1afdd301e33734efb9264
samesamessa <cloverremidore@gmail.com>
2024-10-18 18:17:32 +0900

    #11 메인 페이지 CSS 수정 비교를 위해 주소를 살짝 수정 :5173/1 -> 기존 페이지 :5173/2 -> 페이드 인/아웃 :5173/3 -> 슬라이드

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import LayoutHeader from "./LayoutHeader.jsx";
 import LayoutFooter from "./LayoutFooter.jsx";
 import {QueryClient, QueryClientProvider} from "react-query";
 import ReformPage from "./commponent/RfBoard/ReformPage.jsx";
-import Signup from "./Signup.jsx"; // 게시판 컴포넌트
+import Signup from "./Signup.jsx";
+import MainFade from "./commponent/RfMain/MainFade.jsx";
+import MainSlide from "./commponent/RfMain/MainSlide.jsx"; // 게시판 컴포넌트
 
 
 
@@ -21,7 +23,9 @@ export default function App () {
                 <LayoutHeader/>
                 {/* 페이지 라우팅 설정 */}
                 <Routes>
-                    <Route path="/" element={<MainScroll />} /> {/* 메인 페이지 */}
+                    <Route path="/1" element={<MainScroll/>} /> {/* 메인 페이지 */}
+                    <Route path="/2" element={<MainFade/>} /> {/* 메인 페이지 */}
+                    <Route path="/3" element={<MainSlide/>} /> {/* 메인 페이지 */}
                     <Route path="/posts/*" element={<ReformPage />} /> {/* 게시판 페이지 */}
                     <Route path="/signup" element={<Signup />} />
                 </Routes>
--- src/LayoutHeader.jsx
+++ src/LayoutHeader.jsx
@@ -10,7 +10,6 @@ function SpreadBtn() {
         <DropdownButton className="spread-button" drop="down" title={<FontAwesomeIcon icon={faBars} />}>
             <DropdownItem><Link to="/" className=""><div>홈페이지</div></Link></DropdownItem>
             <DropdownItem><Link to="/posts" className=""><div>게시판</div></Link></DropdownItem>
-            <DropdownItem><Link to="#" className="">일정관리</Link></DropdownItem>
         </DropdownButton>
     );
 }
