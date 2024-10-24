4eb237d5066ae2f8d0c30a642a8e2657b693937a
jincheol <sssm4719@gmail.com>
2024-10-24 22:10:15 +0900

    #4 공용메뉴 디자인 수정

--- src/component/RfLayout/LayoutHeader.jsx
+++ src/component/RfLayout/LayoutHeader.jsx
@@ -1,21 +1,8 @@
-import { Button, Col, DropdownButton, DropdownItem, Nav, Row } from 'react-bootstrap';
+import {Button, Col, DropdownButton, DropdownItem, Nav, Row} from 'react-bootstrap';
 import '../../css/RfLayout/Layout.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import {Link, useNavigate} from "react-router-dom";
-
-function SpreadBtn() {
-    return (
-        <DropdownButton className="spread-button" drop="down" title={<FontAwesomeIcon icon={faBars} />}>
-            <DropdownItem as={Link} to="/">
-                홈페이지
-            </DropdownItem>
-            <DropdownItem as={Link} to="/posts">
-                게시판
-            </DropdownItem>
-        </DropdownButton>
-    );
-}
 
 export default function LayoutHeader({ isLoggedInId, setIsLoggedInId }) {
 
@@ -30,29 +17,42 @@ export default function LayoutHeader({ isLoggedInId, setIsLoggedInId }) {
 
     return (
         <>
-            <Row>
-                <Col xs={12}>
-                    <div className="d-flex justify-content-between align-items-center header-custom">
-                        <SpreadBtn />
-                        <div className="logo justify-content-center">
-                            <img src="/upload/logo.svg" alt="LOGO" className="logo-image" onClick={()=>navigate('/')}/>
-                        </div>
-                        <Nav className="justify-content-end gap-2 nav-custom">
-                            { !isLoggedInId ? (
-                                <>
-                                    <Button variant="outline-secondary" as={Link} to="/login" className="navbtn btn1">로그인</Button>
-                                    <Button variant="outline-secondary" as={Link} to="/signup" className="navbtn btn2">회원가입</Button>
-                                </>
-                            ) : (
-                                <>
-                                    <Button variant="outline-secondary" as={Link} to="/mypage" className="navbtn btn3">내정보</Button>
-                                    <Button variant="outline-secondary" onClick={handleLogout} className="navbtn btn4">로그아웃</Button>
-                                </>
-                            )}
-                        </Nav>
-                    </div>
-                </Col>
-            </Row>
+            <div className="d-flex justify-content-between align-items-center header-custom">
+                <div className="logo justify-content-center">
+                    <img src="/upload/logo.svg" alt="LOGO" className="logo-image" onClick={()=>navigate('/')}/>
+                </div>
+                <Nav className="nav-justified gap-5 nav-custom">
+                    <Button className="nav-btn" as={Link} to="/">
+                        MAIN
+                        <span className="dot"></span>
+                    </Button>
+                    <Button className="nav-btn" as={Link} to="/">
+                        SERVICE
+                        <span className="dot"></span>
+                    </Button>
+                    <Button className="nav-btn" as={Link} to="/posts">
+                        BOARD
+                        <span className="dot"></span>
+                    </Button>
+                    <Button className="nav-btn" as={Link} to="/posts">
+                        ABOUT
+                        <span className="dot"></span>
+                    </Button>
+                </Nav>
+                <DropdownButton className="spread-button" drop="down" title={<FontAwesomeIcon icon={faBars} />}>
+                    { !isLoggedInId ? (
+                        <>
+                            <DropdownItem variant="outline-secondary" as={Link} to="/login" className="navbtn btn1">로그인</DropdownItem>
+                            <DropdownItem variant="outline-secondary" as={Link} to="/signup" className="navbtn btn2">회원가입</DropdownItem>
+                        </>
+                    ) : (
+                        <>
+                            <DropdownItem variant="outline-secondary" as={Link} to="/mypage" className="navbtn btn3">내정보</DropdownItem>
+                            <DropdownItem variant="outline-secondary" onClick={handleLogout} className="navbtn btn4">로그아웃</DropdownItem>
+                        </>
+                    )}
+                </DropdownButton>
+            </div>
         </>
     );
 }
