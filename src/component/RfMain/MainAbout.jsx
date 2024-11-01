a120e721339c38cfb7cc4126f1a603d96bbd1022
KHLee000 <cloverremidore@gmail.com>
2024-11-01 14:38:14 +0900

    #62 기타 수정사항 About css 수정

--- src/component/RfMain/MainAbout.jsx
+++ src/component/RfMain/MainAbout.jsx
@@ -12,13 +12,26 @@ export default function MainAbout() {
 
     return (
         <div className="main-about-container">
-            <h1>조직도</h1>
+            <h1 className={"main-about-h"}>조직도</h1>
+            <hr/>
             <div>
-                <h5>General Manager</h5>
-                <h2>SJC-s</h2>
+                <h5 className={"main-about-h"}>General Manager</h5>
+                <h2 className={"main-about-h"}>SJC-s</h2>
                 <p>
-                    <img src={`/upload/imgAbout/${images[0]}`} alt={`${images[0]}`}/>
-                    <ul>
+                    <img className={"main-about-img"} src={`/upload/imgAbout/${images[0]}`} alt={`${images[0]}`}/>
+                    <ul className={"main-about-ul"}>
+                        <li>다람쥐 헌 쳇바퀴에 타고파</li>
+                        <li>간장공장공장장은강공장장이고된장공장공장장은공공장장이다</li>
+                        <li>대통령은 국회에 출석하여 발언하거나 서한으로 의견을 표시할 수 있다.</li>
+                        <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
+                    </ul>
+                </p>
+                <hr className={"main-about-hr"}/>
+                <h5 className={"main-about-h"}>Assistant Manager</h5>
+                <h2 className={"main-about-h"}>samesamessa</h2>
+                <p>
+                    <img className={"main-about-img"} src={`/upload/imgAbout/${images[1]}`} alt={`${images[1]}`}/>
+                    <ul className={"main-about-ul"}>
                         <li>다람쥐 헌 쳇바퀴에 타고파</li>
                         <li>간장공장공장장은강공장장이고된장공장공장장은공공장장이다</li>
                         <li>대통령은 국회에 출석하여 발언하거나 서한으로 의견을 표시할 수 있다.</li>
@@ -26,23 +39,11 @@ export default function MainAbout() {
                     </ul>
                 </p>
                 <hr/>
-                <h5>Assistant Manager</h5>
-                <h2>samesamessa</h2>
+                <h5 className={"main-about-h"}>Assistant Manager</h5>
+                <h2 className={"main-about-h"}>kyungwook7</h2>
                 <p>
-                    <img src={`/upload/imgAbout/${images[1]}`} alt={`${images[1]}`}/>
-                    <ul>
-                        <li>다람쥐 헌 쳇바퀴에 타고파</li>
-                        <li>간장공장공장장은강공장장이고된장공장공장장은공공장장이다</li>
-                        <li>대통령은 국회에 출석하여 발언하거나 서한으로 의견을 표시할 수 있다.</li>
-                        <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</li>
-                    </ul>
-                </p>
-                <hr/>
-                <h5>Assistant Manager</h5>
-                <h2>kyungwook7</h2>
-                <p>
-                    <img src={`/upload/imgAbout/${images[2]}`} alt={`${images[2]}`}/>
-                    <ul>
+                    <img className={"main-about-img"} src={`/upload/imgAbout/${images[2]}`} alt={`${images[2]}`}/>
+                    <ul className={"main-about-ul"}>
                         <li>다람쥐 헌 쳇바퀴에 타고파</li>
                         <li>간장공장공장장은강공장장이고된장공장공장장은공공장장이다</li>
                         <li>대통령은 국회에 출석하여 발언하거나 서한으로 의견을 표시할 수 있다.</li>
